0973ece13285182af720b09e9998378efe1d336e
juniorgameover <juniorra@gmail.com>
2022-07-01 20:36:22 -0300

    melhoramentos na validação do formulário de cadastro

--- assets/js/valida_form_cadastro.js
+++ assets/js/valida_form_cadastro.js
@@ -3,16 +3,26 @@ let campoSenha = document.querySelector('.input-senha');
 let confSenha = document.querySelector('.input-confirma-senha');
 let botaoCadastrar = document.querySelector('.botao-cadastrar');
 let senhaNaoConfere = document.createElement('span');
+let formulario = document.querySelector('.janela-cadastro__form');
+let labelRadioAdm = document.querySelector('.label-radio-adm');
+
 
 
 /*ESCUTADOR DE EVENTOS NO CAMPO DE CONFIRMAÇÃO DE SENHA */
 
 confSenha.addEventListener('input', function (e) {
     //TESTA SE O CAMPO DE CONFIMAÇÃO DE SENHA É IGUAL AO DE SENHA DURANTE A INSERÇÃO DO CAMPO DE CONFIRMAÇÃO
-    if(campoSenha.value == confSenha.value) {
+    if(campoSenha.value == confSenha.value && confSenha.value != "") {
         botaoCadastrar.disabled = false;
+        senhaNaoConfere.remove();
     } else {
-        botaoCadastrar.disabled = true;
+
+        if(campoSenha.value.length > 0 && confSenha.value.length > 0){
+          botaoCadastrar.disabled = true;
+        senhaNaoConfere.textContent = "As senhas digitadas não são iguais.";
+        senhaNaoConfere.className = 'senha-nao-confere';
+        formulario.insertBefore(senhaNaoConfere, labelRadioAdm);  
+        }   
     }
 })
 
@@ -20,10 +30,16 @@ confSenha.addEventListener('input', function (e) {
 campoSenha.addEventListener('input', function (e) {
 
     //TESTA SE O CAMPO DE CONFIMAÇÃO DE SENHA É IGUAL AO DE SENHA DURANTE A INSERÇÃO DO CAMPO DE CONFIRMAÇÃO
-    if(campoSenha.value == confSenha.value) {
+    if(campoSenha.value == confSenha.value && campoSenha != "") {
         botaoCadastrar.disabled = false;
+        senhaNaoConfere.remove();
+
     } else {
         botaoCadastrar.disabled = true;
-        senhaNaoConfere.textContent = "As senhas digitadas não são iguais."
+        if(campoSenha.value.length > 0 && confSenha.value.length > 0){
+            senhaNaoConfere.textContent = "As senhas digitadas não são iguais.";
+            senhaNaoConfere.className = 'senha-nao-confere';
+            formulario.insertBefore(senhaNaoConfere, labelRadioAdm);
+        } 
     }
 })
